bb81f318bf5466e538a338ad66b2e65d3c1ccee4
garlend <garlend123@gmail.com>
2023-05-04 13:48:42 +0300

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const userName = () => {
-  const name = readlineSync.question("May i have your name? ");
+  const name = readlineSync.question('May i have your name?');
   if (name) {
     console.log(`Hello, ${name}!`);
   }
