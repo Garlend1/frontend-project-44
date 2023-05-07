009fa4549bd27aa587f8d891166b805fc2e97eae
garlend <garlend123@gmail.com>
2023-05-07 14:01:36 +0300

    game-calc changes

--- src/games/game-calc.js
+++ src/games/game-calc.js
@@ -6,26 +6,24 @@ const gameRound = () => {
   const randomOperator = operators[getRandomNumber(2)];
   const num1 = getRandomNumber(10);
   const num2 = getRandomNumber(10);
-  const question =`${num1} ${randomOperator} ${num2}`;
-  
-  let correctAnswer;
-    switch (randomOperator) {
-      case '+':
-        correctAnswer = num1 + num2;
-        break;
-      case '-':
-        correctAnswer = num1 - num2;
-        break;
-      case '*':
-        correctAnswer = num1 * num2;
-        break;
-      default:
-        correctAnswer = 0;
-    }
-    return [question, correctAnswer]
-  };
+  const question = `${num1} ${randomOperator} ${num2}`;
 
-
+  let correctAnswer = 0;
+  switch (randomOperator) {
+    case '+':
+      correctAnswer = num1 + num2;
+      break;
+    case '-':
+      correctAnswer = num1 - num2;
+      break;
+    case '*':
+      correctAnswer = num1 * num2;
+      break;
+    default:
+      correctAnswer = null;
+  }
+  return [question, correctAnswer];
+};
 
 const gameCalc = () => {
   const gameObjective = 'What is the result of the expression?';
